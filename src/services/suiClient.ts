2c5b062d6e59ee769e17cb246513796545f6aa4e
CryptoGolld <121742605+CryptoGolld@users.noreply.github.com>
2025-08-29 13:03:07 +0100

    Delete src directory

--- src/services/suiClient.ts
+++
@@ -1,6 +0,0 @@
-import { SuiClient, getFullnodeUrl } from '@mysten/sui.js/client';
-
-// Create Sui client for testnet
-export const suiClient = new SuiClient({
-  url: getFullnodeUrl('testnet'),
-});
