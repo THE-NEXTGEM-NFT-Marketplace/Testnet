d73eade8d0fe6a90a671a90a995b0a006d2da7d9
CryptoGolld <121742605+CryptoGolld@users.noreply.github.com>
2025-08-29 13:30:20 +0100

    Delete src directory

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import '@suiet/wallet-kit/style.css';
-
-createRoot(document.getElementById("root")!).render(<App />);
